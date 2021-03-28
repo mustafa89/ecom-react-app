3de249d8ac21bb759e692259faa5d1550c5a8b36
mustafa89 <mustafa.mujahid@outlook.com>
2021-03-28 17:54:13 +0200

    :recycle: Make change to main app files for new components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,45 @@ import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import Header from './components/header/header.component.jsx'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+  // set unsubscribe property
+  unsubscribeFromAuth = null;
+
+  // Allows t=us to maintain user login state. User is set to the current user object.
+  // and we can use it through out our app.
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      console.log(userAuth)
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)})
+        });
+      }
+      this.setState({ currentUser: userAuth})
+    })
+  }
+  // As soon as the component unmounts call the above function and refresh user state.
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
   return (
     <div className="App">
-      <Header />
+      <Header currentUser={this.state.currentUser}/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
@@ -18,6 +51,7 @@ function App() {
       </Switch>
     </div>
   );
+}
 }
 
 export default App;
