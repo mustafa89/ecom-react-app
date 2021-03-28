2f979cb812684830a0f309e5ac41e87baa1bd060
mustafa89 <mustafa.mujahid@outlook.com>
2021-03-28 17:51:18 +0200

    :art: add signup page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
-import SignIn from '../../components/signin/signin.component.jsx' 
+import SignIn from '../../components/signin/signin.component.jsx'
+import SignUp from '../../components/sign-up/signup.component.jsx'
 
 const SignInAndSignUpPage = () => (
-    <SignIn className='sign-in-and-sign-up' />
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
+
 )
 export default SignInAndSignUpPage
