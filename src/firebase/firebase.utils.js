2e9ad4801e3e82397dc3afd5af30c528e4283179
mustafa89 <mustafa.mujahid@outlook.com>
2021-03-28 17:52:23 +0200

    :lock: Add Firebase google authentication

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,14 +6,36 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyB7eKgxDDpRscYvs3bxX1TmsEubtW1h5r4',
-  authDomain: 'crossbow-60071.firebaseapp.com',
-  projectId: 'crossbow-60071',
-  storageBucket: 'crossbow-60071.appspot.com',
-  messagingSenderId: '967178290689',
-  appId: '1:967178290689:web:ecfd8d16a45959e8032200',
-  measurementId: 'G-R0WKWSK5X5'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7eKgxDDpRscYvs3bxX1TmsEubtW1h5r4",
+  authDomain: "crossbow-60071.firebaseapp.com",
+  projectId: "crossbow-60071",
+  storageBucket: "crossbow-60071.appspot.com",
+  messagingSenderId: "967178290689",
+  appId: "1:967178290689:web:ecfd8d16a45959e8032200",
+  measurementId: "G-R0WKWSK5X5"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return; //if no user then exit
+  const userRef = firestore.doc(`users/${userAuth.uid}`) //get user with this userid
+  const snapShot = await userRef.get()  //return metadata of the user. If it exists or not and some other stuff
+  if(!snapShot.exits) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user:', error.message)
+    }
+  } return userRef
 }
 
 // Initialize Firebase
@@ -23,7 +45,7 @@ export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
 var provider = new firebase.auth.GoogleAuthProvider()
-
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+provider.setCustomParameters({ prompt: 'select_account'})
+export const signInWithGoogle = () => auth.signInWithRedirect(provider)
 
 export default firebase;
